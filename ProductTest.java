6d00253120ab87b05ee28660f0fd62fb004a1044
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2019-04-08 23:48:56 -0500

    fixed bugs

--- ProductTest.java
+++ ProductTest.java
@@ -5,12 +5,13 @@ import java.util.Random;
 
 class ProductTest {
 
-    private static Product testProduct;
-    private static String title;
-    private static double price;
+    private Product testProduct;
+    private String title;
+    private double price;
 
-    private static final double MAX_PRICE = 10000.0;
+    private final double MAX_PRICE = 10000.0;
 
+    // aneesh saripalli
     @Before
     public void initialize(){
         Random rand = new Random();
@@ -27,16 +28,19 @@ class ProductTest {
         testProduct = new Product(title, price);
     }
 
+    // aneesh saripalli
     @Test
     void getTitle() {
         assertEquals(title, testProduct.getTitle());
     }
 
+    // aneesh saripalli
     @Test
     void getPrice() {
         assertEquals(price, testProduct.getPrice());
     }
 
+    // aneesh saripalli
     @Test
     void equals1() {
         final Product copy = new Product(testProduct.getTitle(), testProduct.getPrice());
